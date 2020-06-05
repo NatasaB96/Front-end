d3ff3dc40b71aaab4b84506e50c8c22daf1b0771
NatasaB96 <63608619+NatasaB96@users.noreply.github.com>
2020-06-05 14:54:39 +0200

    Add files via upload

---
+++ Business/index.php
@@ -0,0 +1,163 @@
+<?php include "partials/header.php"; ?>
+
+<!-- header -->
+<header>
+    <h1>Trusted and <br> professional advisers</h1>
+    <h3>For your business</h3>
+    <p>Showcase your Profile to the world using online CV builder and Get Hired Faster</p>
+    <button type="button">About us</button>
+</header>
+
+<!-- headers -->
+<section class="container headings">
+    <div class="col">
+        <h2>01</h2>
+    </div>
+    <div class="col">
+        <h3>About us</h3>
+        <h2><span>WE CAN</span> HELP YOUR BUSINESS GROW</h2>
+        <h4>Get your business in 4 easy steps</h4>
+    </div>
+    
+</section>
+
+<!-- about -->
+<section class="about">
+    <div class="col"></div>
+    <div class="col">
+        <h2>EVERYTHING YOU NEED IN ONE SOLUTION</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi vero, illum ex ratione quos libero culpa ducimus consequuntur id necessitatibus dolore praesentium natus reiciendis iusto voluptatibus veniam autem nostrum. Quam enim, maiores libero minus culpa id accusamus rerum ipsa, sapiente recusandae, maxime est fugit praesentium. Accusantium, animi nulla mollitia quisquam!</p>
+    </div>
+</section>
+
+
+<!-- headers -->
+<section class="container headings">
+    <div class="col">
+        <h2>02</h2>
+    </div>
+    <div class="col">
+        <h3>Services</h3>
+        <h2><span>HOW TO PROVIDE</span> TOOLS THAT HELP ANYONE</h2>
+        <h4>Get your business in 4 easy steps</h4>
+    </div>
+    
+</section>
+
+<!-- services -->
+<section class="services container">
+    <div class="col">
+        <i class="far fa-building"></i>
+        <h3>Marketing</h3>
+        <hr>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis, assumenda, aspernatur! Animi, laborum cum at ullam tempora repellendus fuga doloremque recusandae, molestiae atque doloribus, illum est. Nisi est ad vero ipsum, eaque in blanditiis nulla, voluptatum, reiciendis similique eligendi tempore, consectetur? Officia nihil similique, provident maiores quae officiis eius voluptas.</p>
+    </div>
+    
+     <div class="col">
+        <i class="far fa-chart-bar"></i>
+        <h3>Marketing</h3>
+        <hr>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis, assumenda, aspernatur! Animi, laborum cum at ullam tempora repellendus fuga doloremque recusandae, molestiae atque doloribus, illum est. Nisi est ad vero ipsum, eaque in blanditiis nulla, voluptatum, reiciendis similique eligendi tempore, consectetur? Officia nihil similique, provident maiores quae officiis eius voluptas.</p>
+    </div>
+    
+     <div class="col">
+        <i class="fas fa-bullseye"></i>
+        <h3>Marketing</h3>
+        <hr>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis, assumenda, aspernatur! Animi, laborum cum at ullam tempora repellendus fuga doloremque recusandae, molestiae atque doloribus, illum est. Nisi est ad vero ipsum, eaque in blanditiis nulla, voluptatum, reiciendis similique eligendi tempore, consectetur? Officia nihil similique, provident maiores quae officiis eius voluptas.</p>
+    </div>
+    
+     <div class="col">
+        <i class="fas fa-map-signs"></i>
+        <h3>Marketing</h3>
+        <hr>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis, assumenda, aspernatur! Animi, laborum cum at ullam tempora repellendus fuga doloremque recusandae, molestiae atque doloribus, illum est. Nisi est ad vero ipsum, eaque in blanditiis nulla, voluptatum, reiciendis similique eligendi tempore, consectetur? Officia nihil similique, provident maiores quae officiis eius voluptas.</p>
+    </div>
+    
+     <div class="col">
+        <i class="fas fa-filter"></i>
+        <h3>Marketing</h3>
+        <hr>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis, assumenda, aspernatur! Animi, laborum cum at ullam tempora repellendus fuga doloremque recusandae, molestiae atque doloribus, illum est. Nisi est ad vero ipsum, eaque in blanditiis nulla, voluptatum, reiciendis similique eligendi tempore, consectetur? Officia nihil similique, provident maiores quae officiis eius voluptas.</p>
+    </div>
+    
+     <div class="col">
+        <i class="fas fa-hand-holding-usd"></i>
+        <h3>Marketing</h3>
+        <hr>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis, assumenda, aspernatur! Animi, laborum cum at ullam tempora repellendus fuga doloremque recusandae, molestiae atque doloribus, illum est. Nisi est ad vero ipsum, eaque in blanditiis nulla, voluptatum, reiciendis similique eligendi tempore, consectetur? Officia nihil similique, provident maiores quae officiis eius voluptas.</p>
+    </div>
+</section>
+
+<!-- personal business -->
+<div class="personalBusiness">
+    <!-- headers -->
+    <section class="container headings">
+        <div class="col">
+            <h2>03</h2>
+        </div>
+        <div class="col">
+            <h3>What we done</h3>
+            <h2><span>CREATE</span> PERSONALISED BUSINESS</h2>
+            <h4>Get your business in 4 easy steps</h4>
+            <button type="button">About us</button>
+        </div>
+    </section>
+    
+    <div class="counter container">
+        <div class="col">
+            <h2>18</h2>
+            <h4>Nominations</h4>
+            <hr>
+        </div>
+        
+        <div class="col">
+            <h2>5</h2>
+            <h4>Awards</h4>
+            <hr>
+        </div>
+        
+        <div class="col">
+            <h2>45</h2>
+            <h4>Agencies</h4>
+            <hr>
+        </div>
+    </div>
+</div>
+
+<!-- headers-->
+ <section class="container headings">
+        <div class="col">
+            <h2>04</h2>
+        </div>
+        <div class="col">
+            <h3>Our team</h3>
+            <h2><span>WE HAVE A PROFESSIONAL</span> TEAM OF BUSINESS ANALYSIS</h2>
+            <h4>Get your business in 4 easy steps</h4>
+        </div>
+</section>
+   
+<!-- team-->
+<section class="team container">
+    <div class="col">
+        <img src="img/team1.jpg" alt="">
+        <h4>James Douglas</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis labore odit, architecto eligendi quas placeat.</p>
+    </div>
+    
+    <div class="col">
+        <img src="img/team2.jpg" alt="">
+        <h4>Peter Green</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis labore odit, architecto eligendi quas placeat.</p>
+    </div>
+    
+    <div class="col">
+        <img src="img/team3.jpg" alt="">
+        <h4>Ela Williams</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis labore odit, architecto eligendi quas placeat.</p>
+    </div>
+</section>
+    
+
+<?php include "partials/footer.php"; ?>
+
---
+++ Business/partials/footer.php
@@ -0,0 +1,28 @@
+<footer>
+    <div class="container">
+        <div class="col">
+            <a href="" class="logo">BUSINESS <br> <span>Marketing & consulting</span></a>
+        </div>
+        
+        <div class="col">
+            <i class="fas fa-map-marker-alt"></i>
+            <p>Main street 2, Novi Sad</p>
+            <p>Serbia</p>
+        </div>
+        
+        <div class="col">
+            <i class="fas fa-envelope"></i>
+            <p>info@marketing.com</p>
+            <p>office@marketing.com</p>
+        </div>
+        
+        <div class="col">
+            <i class="fas fa-phone"></i>
+            <p>062 165 16 65</p>
+            <p>061 123 13 45</p>
+        </div>
+    </div>
+</footer>
+
+</body>
+</html>
