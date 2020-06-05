fdc8398b7fec493944da4cd7ad28e76e04b89d05
NatasaB96 <63608619+NatasaB96@users.noreply.github.com>
2020-06-05 14:37:47 +0200

    Add files via upload

---
+++ Webcon-shop/index.php
@@ -0,0 +1,43 @@
+<?php include "partials/header.php"; ?>
+
+<!-- Header -->
+<header>
+    <h4>Woman collection</h4>
+    <h2><span>Show</span>Your <br> Personal <span>Style</span></h2>
+    <p>Lorem ipsum dolor sit amet, consectetur <br> adipisicing elit. Sint, voluptate.</p>
+    <button type="button">View collection</button>
+</header>
+
+<!-- fatured products -->
+<section class="container featuredProducts">
+        <h2 class="headings">Featured products</h2>
+        <div class="row">
+            <div class="col">
+                <img src="img/products1.jpg" alt="">
+                <h3>Nike</h3>
+                <div class="price">
+                    <p>$120</p>
+                    <p>$100</p>
+                </div>
+            </div>
+            <div class="col">
+                <img src="img/products2.jpg" alt="">
+                <h3>Iphone</h3>
+                <div class="price">
+                    <p>$120</p>
+                    <p>$100</p>
+                </div>
+            </div>
+            <div class="col">
+                <img src="img/products3.jpg" alt="">
+                <h3>Gloves</h3>
+                <div class="price">
+                    <p>$120</p>
+                    <p>$100</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+<?php include "partials/footer.php"; ?>
---
+++ Webcon-shop/product.php
@@ -0,0 +1,38 @@
+<?php include "partials/header.php"; ?>
+
+<!-- headers -->
+    <section class="headers">
+        <h2>Product</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, labore.</p>
+    </section>
+    
+<!-- headers -->
+<section class="product container">
+    <article>
+        <img src="img/products1.jpg" alt="">
+    </article>
+    
+    <aside>
+        <h3>Nike</h3>
+        <h4>$139.00</h4>
+        <p>Categgory: <span>Shoes</span></p>
+        <p>Availibity: <span>In Stock</span>
+        </p>
+        <hr>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum reprehenderit rem omnis error illo, quam nulla ex consequatur amet sapiente delectus provident dolorum cumque, quae alias facilis cum unde, tempore. Aliquam praesentium dignissimos aspernatur sapiente ipsam repellat. Suscipit, est, perferendis?</p>
+        <form>
+            <label>Quantify</label>
+            <select>
+                <option value="quantity">1</option>
+                <option value="quantity">2</option>
+                <option value="quantity">3</option>
+            </select>
+            <br>
+            <button type="button">Add to cart</button>
+            <span><i class="far fa-gem"></i></span>
+            <span><i class="far fa-heart"></i></span>
+        </form>
+    </aside>
+</section>
+
+<?php include "partials/footer.php"; ?>
